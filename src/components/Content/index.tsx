0b9c0f10ed6e16ea0e21dad742fee9d4ce5e962e
Gustavo <avoguga@gmail.com>
2022-09-17 17:28:27 -0300

    separando content por sections e adicionando frescurinhas

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -1,9 +1,40 @@
-import { Container } from "./styles";
+import { Container, FirstSection, SecondSection, ThirdSection } from "./styles";
 
 export function Content() {
-
   return (
-    <Container />
+    <Container>
+      <FirstSection>
+        <h2>Teste o plugin de acessibilidade!</h2>
+        <br />
+        <p>
+          Caso não entenda como usa-lo de primeira, acesse o{" "}
+          <span>
+            {" "}
+            <a
+              href="https://github.com/avoguga/accessibility-toolkit"
+              target="_blank"
+            >
+              GitHub do projeto!
+            </a>{" "}
+          </span>
+          Leia o Readme e veja o video de uso!
+        </p>
+        <br />
+        <h2>Teste o sign in do Google!</h2>
+        <br />  
+        <h2>Clique no DALE!</h2>
+        <br />
+        <h3>Acesse minhas redes:</h3>
+        <br />
+        <p>Linkedin</p>
+        <p>Twitter</p>
+        <p>Instagram</p>
+        <p>Medium</p>
+
+      </FirstSection>
+      <SecondSection>segundo</SecondSection>
+      <ThirdSection>terceiro</ThirdSection>
+    </Container>
   );
 }
 
--- src/components/Content/styles.ts
+++ src/components/Content/styles.ts
@@ -5,7 +5,24 @@ export const Container = styled.div`
   background-color: whitesmoke;
 
   display: flex;
-  flex-direction: column;
-  justify-content: center;
+  justify-content: space-around;
   align-items: center;
+`;
+
+export const FirstSection = styled.div`
+  height: 100%;
+  width: 100%;
+  padding: 10px;
+  border-right: 2px solid black;
+`;
+
+export const SecondSection = styled.div`
+  height: 100%;
+  width: 100%;
+`;
+
+export const ThirdSection = styled.div`
+  height: 100%;
+  width: 100%;
+  border-left: 2px solid black;
 `;
