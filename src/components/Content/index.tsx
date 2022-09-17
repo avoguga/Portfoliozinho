84457eaf5883c826d270816bd3201fd4329baab3
Gustavo <avoguga@gmail.com>
2022-09-17 04:44:12 -0300

    ADICIONa OS DALE DALE

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -1,9 +1,43 @@
-import { Container } from "./styles";
+import { useEffect, useState } from "react";
+import { Container, SoundButton, PauseButton, StopButton } from "./styles";
+import Sound from "../../assets/dale.mp3";
 
 export function Content() {
+  const audioTune = new Audio(Sound);
 
+  const [playInLoop, setPlayInLoop] = useState(false);
+
+  useEffect(() => {
+    audioTune.load();
+  }, []);
+
+  useEffect(() => {
+    audioTune.loop = playInLoop;
+  }, [playInLoop]);
+
+  /** Toca a musica */
+  const playSound = () => {
+    audioTune.play();
+  };
+
+  /** Pausa a musica */
+  const pauseSound = () => {
+    audioTune.pause();
+  };
+
+  /** Para a musica */
+  const stopSound = () => {
+    audioTune.pause();
+    audioTune.currentTime = 0;
+  }
+
+  SoundButton
   return (
-    <Container />
+    <Container>
+      <SoundButton onClick={playSound}>DALE</SoundButton>
+      <PauseButton onClick={pauseSound}>PAUSA O DALE</PauseButton>
+      <StopButton onClick={stopSound}>PARA O DALE</StopButton>
+    </Container>
   );
 }
 
--- src/components/Content/styles.ts
+++ src/components/Content/styles.ts
@@ -5,6 +5,132 @@ export const Container = styled.div`
   background-color: whitesmoke;
 
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
+`;
+
+// TO DO - REFATORAR ISSO NÉ IRMÃO TÁ FEIAO ESSA REPETIÇÃO AI
+
+export const SoundButton = styled.button`
+  transition: background-color 0.3s, box-shadow 0.3s;
+  margin: 10px;
+  padding: 12px 16px 12px 42px;
+  border: none;
+  border-radius: 3px;
+  box-shadow: 0 -1px 0 rgba(0, 0, 0, 0.04), 0 1px 1px rgba(0, 0, 0, 0.25);
+
+  color: #000;
+  font-size: 20px;
+  font-weight: bold;
+ 
+
+  background-image: url(https://www.svgrepo.com/show/393821/sound.svg);
+  background-color: white;
+  background-repeat: no-repeat;
+  background-position: 7px 10px;
+  background-size: 25px;
+
+  &:hover {
+    box-shadow: 0 -1px 0 rgba(0, 0, 0, 0.04), 0 2px 4px rgba(0, 0, 0, 0.25);
+  }
+
+  &:active {
+    background-color: #eeeeee;
+  }
+
+  &:focus {
+    outline: none;
+    box-shadow: 0 -1px 0 rgba(0, 0, 0, 0.04), 0 2px 4px rgba(0, 0, 0, 0.25),
+      0 0 0 3px #c8dafc;
+  }
+
+  &:disabled {
+    filter: grayscale(100%);
+    background-color: #ebebeb;
+    box-shadow: 0 -1px 0 rgba(0, 0, 0, 0.04), 0 1px 1px rgba(0, 0, 0, 0.25);
+    cursor: not-allowed;
+  }
+`;
+
+export const PauseButton = styled.button`
+  transition: background-color 0.3s, box-shadow 0.3s;
+  margin: 10px;
+  padding: 12px 16px 12px 42px;
+  border: none;
+  border-radius: 3px;
+  box-shadow: 0 -1px 0 rgba(0, 0, 0, 0.04), 0 1px 1px rgba(0, 0, 0, 0.25);
+
+  color: #000;
+  font-size: 20px;
+  font-weight: bold;
+ 
+
+  background-image: url(https://www.svgrepo.com/show/44880/pause.svg);
+  background-color: white;
+  background-repeat: no-repeat;
+  background-position: 7px 10px;
+  background-size: 25px;
+
+  &:hover {
+    box-shadow: 0 -1px 0 rgba(0, 0, 0, 0.04), 0 2px 4px rgba(0, 0, 0, 0.25);
+  }
+
+  &:active {
+    background-color: #eeeeee;
+  }
+
+  &:focus {
+    outline: none;
+    box-shadow: 0 -1px 0 rgba(0, 0, 0, 0.04), 0 2px 4px rgba(0, 0, 0, 0.25),
+      0 0 0 3px #c8dafc;
+  }
+
+  &:disabled {
+    filter: grayscale(100%);
+    background-color: #ebebeb;
+    box-shadow: 0 -1px 0 rgba(0, 0, 0, 0.04), 0 1px 1px rgba(0, 0, 0, 0.25);
+    cursor: not-allowed;
+  }
+`;
+
+export const StopButton = styled.button`
+  transition: background-color 0.3s, box-shadow 0.3s;
+  margin: 10px;
+  padding: 12px 16px 12px 42px;
+  border: none;
+  border-radius: 3px;
+  box-shadow: 0 -1px 0 rgba(0, 0, 0, 0.04), 0 1px 1px rgba(0, 0, 0, 0.25);
+
+  color: #000;
+  font-size: 20px;
+  font-weight: bold;
+ 
+
+  background-image: url(https://www.svgrepo.com/show/53664/stop.svg);
+  background-color: white;
+  background-repeat: no-repeat;
+  background-position: 7px 10px;
+  background-size: 25px;
+
+  &:hover {
+    box-shadow: 0 -1px 0 rgba(0, 0, 0, 0.04), 0 2px 4px rgba(0, 0, 0, 0.25);
+  }
+
+  &:active {
+    background-color: #eeeeee;
+  }
+
+  &:focus {
+    outline: none;
+    box-shadow: 0 -1px 0 rgba(0, 0, 0, 0.04), 0 2px 4px rgba(0, 0, 0, 0.25),
+      0 0 0 3px #c8dafc;
+  }
+
+  &:disabled {
+    filter: grayscale(100%);
+    background-color: #ebebeb;
+    box-shadow: 0 -1px 0 rgba(0, 0, 0, 0.04), 0 1px 1px rgba(0, 0, 0, 0.25);
+    cursor: not-allowed;
+  }
 `;
